584b8e392999ef9c47ff11ec66eb9f58396350f4
Szandor72 <szandor72@gmail.com>
2020-02-29 19:17:10 +0100

    first version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
-var express = require('express');
+const uniqueRandomArray = require("unique-random-array");
+const data = require("../data/quotations.json");
+const randomArray = uniqueRandomArray(data);
+
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  const randomEntry = randomArray();
+  const title = randomEntry.title;
+  const content = randomEntry.content;
+
+  let topic;
+  if (title.indexOf(":") > -1) {
+    topic = title.substring(0, title.indexOf(":"));
+  } else {
+    topic = "";
+  }
+
+  let sentences = uniqueRandomArray(content.split(". "));
+  let quotation = sentences();
+
+  if (quotation.indexOf("?") > -1) {
+    quotation = quotation.substring(quotation.indexOf("?"), quotation.length);
+    quotation = quotation.replace("? ", "");
+    quotation = quotation.trim() + ".";
+  }
+
+  if (!topic) {
+    topic = title;
+  }
+
+  res.render("index", {
+    title: topic,
+    heading: title,
+    quotation: quotation
+  });
 });
 
 module.exports = router;
